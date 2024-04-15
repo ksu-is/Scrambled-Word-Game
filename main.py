4db6762835cb7afa4e16f4f9408d691057a39421
MakennaHogan <156006903+MakennaHogan@users.noreply.github.com>
2024-04-15 10:37:52 -0400

    Update main.py
    Got rid of the welcome screen code.

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 
-# This is the welcome screen code
-def welcome():
-    print("Welcome to the Word Scrambled Game!")
-    print("There are three levels, to move through each level you will have to unscramble each word correctly.")
-    print("Enjoy unscrambling!")
-    input("Press Enter to Start!")
-print()
-
 import random 
 
 # This is the word bank for level one
