e726f2dc0094d09ca70379f07f537cb3740f0fe6
MakennaHogan <156006903+MakennaHogan@users.noreply.github.com>
2024-04-16 13:34:01 -0400

    Update main.py
    Had to change "Jumbled" word to "Scrambled" word because that is the title of our game.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 first_word = ['fun', 'run', 'tan']
 a = random.choice(first_word)
 jumble = ''.join(random.sample(a,len(a)))
-print(f"the jumbled word is {jumble}")
+print(f"The Scrambled Word Is {jumble}")
 
 
 user_word = input("Guess the word: ")
@@ -20,12 +20,12 @@ breakpoint
 second_word = ['alert', 'brief', 'cycle']
 b= random.choice(second_word)
 jumbletwo= ''.join(random.sample(b,len(b)))
-print(f"the jumbled word is {jumbletwo}")
+print(f"The Scrambled Word Is {jumbletwo}")
 
 user_word= input("Guess the word: ")
 
 if user_word == second_word:
-    print("incorrect!")
+    print("Incorrect!")
 else:
     print("Correct! Move on to the last level!")
 
@@ -34,7 +34,7 @@ breakpoint
 third_word = ['computer', 'average', 'information']
 c = random.choice(third_word)
 jumblethree = ''.join(random.sample(c,len(c)))
-print(f"the jumbled word us {jumblethree}")
+print(f"The Scrambled Word Is {jumblethree}")
 
 user_word = input ("Guess the word: ")
 
